7ee540cf93bf2b336b243a5d98b52c94aa9a8fb8
Robinson Garcia <rgarciameldz@gmail.com>
2023-09-06 08:39:22 -0600

    updating the selector for modal body

--- webroot/js/add-edit-modal.js
+++ webroot/js/add-edit-modal.js
@@ -4,7 +4,7 @@ $('.btn-modal').on('click', function () {
 	var title = $(this).attr('modal-title');
 	var dataURL = $(this).attr('href');
     $('.modal-title').text(title);
-	$('.modal-body').load(dataURL,function(){
+	$('#addEditModal .modal-body').load(dataURL,function(){
 		$('#addEditModal').modal({show:true});
 	});
 })
